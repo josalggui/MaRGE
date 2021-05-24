91d50cb07d301398bdbff36a6aa114463a2be3cb
iolan <yvivesgilabert@gmail.com>
2021-05-24 18:19:43 +0200

    correction gradients spinEcho

--- seq/spinEcho.py
+++ seq/spinEcho.py
@@ -7,6 +7,33 @@ import matplotlib.pyplot as plt
 import pdb
 st = pdb.set_trace
 
+def trapezoid(plateau_a, total_t, ramp_t, ramp_pts, total_t_end_to_end=True, base_a=0):
+    """Helper function that just generates a Numpy array starting at time
+    0 and ramping down at time total_t, containing a trapezoid going from a
+    level base_a to plateau_a, with a rising ramp of duration ramp_t and
+    sampling period ramp_ts."""
+
+    # ramp_pts = int( np.ceil(ramp_t/ramp_ts) ) + 1
+    rise_ramp_times = np.linspace(0, ramp_t, ramp_pts)
+    rise_ramp = np.linspace(base_a, plateau_a, ramp_pts)
+
+    # [1: ] because the first element of descent will be repeated
+    descent_t = total_t - ramp_t if total_t_end_to_end else total_t
+    t = np.hstack([rise_ramp_times, rise_ramp_times[:-1] + descent_t])
+    a = np.hstack([rise_ramp, np.flip(rise_ramp)[1:]])
+    return t, a
+
+
+def trap_cent(centre_t, plateau_a, trap_t, ramp_t, ramp_pts, base_a=0):
+    """Like trapezoid, except it generates a trapezoid shape around a centre
+    time, with a well-defined area given by its amplitude (plateau_a)
+    times its time (trap_t), which is defined from the start of the
+    ramp-up to the start of the ramp-down, or (equivalently) from the
+    centre of the ramp-up to the centre of the ramp-down. All other
+    parameters are as for trapezoid()."""
+    t, a = trapezoid(plateau_a, trap_t, ramp_t, ramp_pts, False, base_a)
+    return t + centre_t - (trap_t + ramp_t)/2, a
+
 def spin_echo(self, plotSeq):
 #                    plot_rx=True, init_gpa=False,
 #                    dbg_sc=0.5, # set to 0 to avoid RF debugging pulses in each RX window, otherwise amp between 0 or 1
@@ -32,6 +59,10 @@ def spin_echo(self, plotSeq):
     echo_duration=self.echo_duration
     readout_duration=self.readout_duration
     rx_period=self.rx_period
+    shim_x: int = self.shim[0]
+    shim_y: int = self.shim[1]
+    shim_z: int = self.shim[2]
+    trap_ramp_pts=5
                      
     """
     readout gradient: x
@@ -44,6 +75,9 @@ def spin_echo(self, plotSeq):
                     
     if rf_pi_duration is None:
         rf_pi_duration = 2 * rf_pi2_duration
+    
+    phase_amps = np.linspace(self.phase_start_amp, -self.phase_start_amp, self.echos_per_tr)
+    slice_amps = np.linspace(self.slice_start_amp, -self.slice_start_amp, self.trs)
 
     # create appropriate waveforms for each echo, based on start time, echo index and TR index
     # note: echo index is 0 for the first interval (90 pulse until first 180 pulse) thereafter 1, 2 etc between each 180 pulse
@@ -88,6 +122,40 @@ def spin_echo(self, plotSeq):
             return np.array([tstart + (echo_duration - readout_duration)/2, tstart + (echo_duration + readout_duration)/2 ]), np.array([1, 0])
         else:
             return np.array([tstart]), np.array([0]) # keep on zero otherwise
+            
+            
+    def readout_grad_wf(tstart, echo_idx):
+        if echo_idx == 0:
+            return trap_cent(tstart + self.echo_duration*3/4, self.readout_amp, self.readout_grad_duration/2,
+                             self.trap_ramp_duration, trap_ramp_pts)
+        else:
+            return trap_cent(tstart + self.echo_duration/2, self.readout_amp, self.readout_grad_duration,
+                             self.trap_ramp_duration, trap_ramp_pts)
+
+    def phase_grad_wf(tstart, echo_idx):
+        t1, a1 = trap_cent(tstart + (self.echo_duration - self.phase_grad_interval)/2, phase_amps[echo_idx-1], self.phase_grad_duration,
+                           self.trap_ramp_duration, trap_ramp_pts)
+        t2, a2 = trap_cent(tstart + (self.echo_duration + self.phase_grad_interval)/2, -phase_amps[echo_idx-1], self.phase_grad_duration,
+                           self.trap_ramp_duration, trap_ramp_pts)
+        if echo_idx == 0:
+            return np.array([tstart]), np.array([0]) # keep on zero otherwise
+        elif echo_idx == self.echos_per_tr: # last echo, don't need 2nd trapezoids
+            return t1, a1
+        else: # otherwise do both trapezoids
+            return np.hstack([t1, t2]), np.hstack([a1, a2])
+
+    def slice_grad_wf(tstart, echo_idx, tr_idx):
+        t1, a1 = trap_cent(tstart + (self.echo_duration - self.phase_grad_interval)/2, slice_amps[tr_idx], self.phase_grad_duration,
+                           self.trap_ramp_duration, trap_ramp_pts)
+        t2, a2 = trap_cent(tstart + (self.echo_duration + self.phase_grad_interval)/2, -slice_amps[tr_idx], self.phase_grad_duration,
+                           self.trap_ramp_duration, trap_ramp_pts)
+        if echo_idx == 0:
+            return np.array([tstart]), np.array([0]) # keep on zero otherwise
+        elif echo_idx == self.echos_per_tr: # last echo, don't need 2nd trapezoids
+            return t1, a1
+        else: # otherwise do both trapezoids
+            return np.hstack([t1, t2]), np.hstack([a1, a2])
+
 
     expt = ex.Experiment(lo_freq=lo_freq, rx_t=rx_period, init_gpa=init_gpa, gpa_fhdo_offset_time=(1 / 0.2 / 3.1))
     # gpa_fhdo_offset_time in microseconds; offset between channels to
@@ -102,11 +170,17 @@ def spin_echo(self, plotSeq):
             tx_gate_t, tx_gate_a = tx_gate_wf(global_t, echo)
             readout_t, readout_a = readout_wf(global_t, echo)
             rx_gate_t, rx_gate_a = readout_wf(global_t, echo)
+            readout_grad_t, readout_grad_a = readout_grad_wf(global_t, echo)
+            phase_grad_t, phase_grad_a = phase_grad_wf(global_t, echo)
+            slice_grad_t, slice_grad_a = slice_grad_wf(global_t, echo, tr)
 
             global_t += echo_duration
 
             expt.add_flodict({
                 'tx0': (tx_t, tx_a),
+                'grad_vx': (readout_grad_t, shim_x),
+                'grad_vy': (phase_grad_t, shim_y),
+                'grad_vz': (slice_grad_t, shim_z),
                 'rx0_en': (readout_t, readout_a),
                 'tx_gate': (tx_gate_t, tx_gate_a),
                 'rx_gate': (rx_gate_t, rx_gate_a),
