787e6be104d16af0f5a29f7a38670b320fb2ba7f
yvives <yolanda.vives@physiomri.com>
2022-02-28 08:29:02 +0100

    I fixed some mistakes for the IR sequence. Now it calulates the T1 and M0 with a fitting to 1 and 2 exponentials. It shows the results in the shell and plots the results in a figure. It also save all data in the rawData

--- seq_standalone/InversionRecovery_Ajuste.py
+++ seq_standalone/InversionRecovery_Ajuste.py
@@ -1,5 +1,6 @@
 """
 @author: Teresa
+@modifield: J.M. algarín, february 25th 2022
 """
 import sys
 sys.path.append('../marcos_client')
@@ -7,25 +8,42 @@ import experiment as ex
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from datetime import date,  datetime 
+import os
+from scipy.io import savemat
 
 def inversion_recovery_TGN(
-    init_gpa= True,                 
-    lo_freq=3.076, 
+    init_gpa= False,                 
+    lo_freq=3.07264, 
     rf_amp=0.3, 
     rf_pi_duration=None, 
-    rf_pi2_duration=30, 
-    tr = 1000*1e3, 
-    t_ini =50*1e3,
-    t_fin = 500*1e3,
-    N = 20,
-    BW=60*1e-3, 
+    rf_pi2_duration=32, 
+    tr = 4,                 # s
+    t_ini = 10e-3,      # s
+    t_fin = 3,             # s
+    N = 10):                
+    
+    BW=60*1e-3,                     # MHz
     n_rd=120,
-   twoT1 = 0             ,  
-    plotSeq =0):
+    plotSeq =0
+    point = 10
     
     if rf_pi_duration is None:
         rf_pi_duration = 2 * rf_pi2_duration
-        
+    
+    rawData = {}
+    rawData['larmorFreq'] = lo_freq*1e6
+    rawData['rfAmp'] = rf_amp
+    rawData['rfInTime'] = rf_pi_duration
+    rawData['rfExtime'] = rf_pi2_duration
+    rawData['TR'] = tr*1e-6
+    rawData['tInvIni'] = t_ini*1e-6
+    rawData['tInvFin'] = t_fin*1e-6
+    rawData['nRepetitions'] = N
+    rawData['bw'] = BW*1e6
+    rawData['nRD'] = n_rd
+    rawData['point'] = point
+    
     t_adq = n_rd/BW
     
     tx_gate_pre = 15 # us, time to start the TX gate before each RF pulse begins
@@ -36,11 +54,10 @@ def inversion_recovery_TGN(
     expt = ex.Experiment(lo_freq=lo_freq, rx_t=rx_period, init_gpa=init_gpa, gpa_fhdo_offset_time=(1 / 0.2 / 3.1))
 
     t_start = 20 
-    t = t_ini
+    t = t_ini*1e6
     blk_delay = 300
-    tIR = np.geomspace(t_ini, t_fin, N)
-    #tIR = np.linspace(t_ini, t_fin, N)
-    
+    tIR = np.geomspace(t_ini, t_fin, N)*1e6             # in us to be used in the sequence
+    tr = tr*1e6
     for t in tIR:
         
         tx_t = np.array([t_start, t_start+rf_pi_duration,t_start+t+rf_pi2_duration/2,t_start+t+3*rf_pi2_duration/2])
@@ -78,6 +95,9 @@ def inversion_recovery_TGN(
 
 
 # Representar Secuencia o tomar los datos.
+    tIR1 = np.geomspace(t_ini, t_fin, N)
+    tIR2 = np.geomspace(t_ini, t_fin, 10*N)
+    tIR
     if plotSeq==1:                
         expt.plot_sequence()
         plt.show()
@@ -87,48 +107,83 @@ def inversion_recovery_TGN(
         rxd, msgs = expt.run()
         print('End')
         data = rxd['rx0']*13.788
-        dataIndiv = np.reshape(data,  (N, n_rd))
         expt.__del__()
-#        plt.figure(1)
-#        plt.plot(np.abs(dataIndiv[0, :]),  'o')
-#        plt.plot(np.abs(dataIndiv[1, :]),  'go')
-#        plt.plot(np.abs(dataIndiv[2, :]),  'bo')
-        #Data analysis
-        results = []
-        cont = 0
-        for t in tIR:
-            if t == t_ini: 
-                results = [[t, max(np.real(dataIndiv[cont]))]]
-            else: 
-                results = np.append(results, [[t, max(np.real(dataIndiv[cont]))]],axis=0)
-            cont += 1
-        results[:,1]=results[:,1]/np.max(results[:,1])
-        plt.figure(2)
-        plt.plot(results[:, 0]*1e-3, results[:, 1], 'o')
-        if twoT1 == 0:
-            T1 = results[:,0][np.argmin(results[:,1])]*1e-3/np.log(2)
-            plt.suptitle('T1 = '+str(T1))
-        else:
-            fitData = curve_fit(lambda t,a,b, c, d: a*(1-2*np.exp(-t/b))+c*(1-2*np.exp(-t/d)),  results[:, 0],  results[:, 1] ,  p0=(0.9, 80*1e3,0.1, 50*1e3))
-            m1= fitData[0][0]
-            t1_1= fitData[0][1]
-            m2= fitData[0][2]
-            t1_2= fitData[0][3]
-            fit1=m1*(1-2*np.exp(results[:, 0]/t1_1))
-            leg1 = 'T1 = '+ str(np.round(t1_1*1e-3))+ 'ms'
-            fit2=m2*(1-2*np.exp(results[:, 0]/t1_2))
-            leg2 = 'T1 = '+ str(np.round(t1_2*1e-3))+ 'ms'
-            plt.plot(results[:, 0]*1e-3, fit1, 'b-', label=leg1)
-            plt.plot(results[:, 0]*1e-3, fit2, 'g-', label=leg2)
-            print('m1', m1)
-            print('t1_1', t1_1)
-            print('m2', m2)
-            print('t1_2',t1_2)
+        rawData['fullData'] = data
+        dataIndiv = np.reshape(data,  (N, n_rd))
+        dataIndiv = np.real(dataIndiv[:, point]*np.exp(-1j*(np.angle(dataIndiv[0, 1])+np.pi)))
+        results = np.transpose(np.array([tIR1, dataIndiv]))
+        rawData['signalVsTime'] = results
+        
+        # For 1 component
+        fitData1, xxx = curve_fit(func1, results[:, 0],  results[:, 1])
+        print('For one component:')
+        print('mA', round(fitData1[0], 1))
+        print('T1', round(fitData1[1]*1e3), ' ms')
+        rawData['T11'] = fitData1[1]*1e3
+        rawData['M1'] = fitData1[0]
+        
+        # For 2 components
+        fitData2, xxx = curve_fit(func2, results[:, 0],  results[:, 1])
+        print('For two components:')
+        print('Ma', round(fitData2[0], 1))
+        print('Mb', round(fitData2[2], 1))
+        print('T1a', round(fitData2[1]*1e3), ' ms')
+        print('T1b', round(fitData2[3]*1e3), ' ms')
+        rawData['T12'] = [fitData2[1], fitData2[3]]
+        rawData['M2'] = [fitData2[0], fitData2[2]]
+        
+#        # For 3 components
+#        fitData3, xxx = curve_fit(func3, results[:, 0],  results[:, 1])
+#        print('For two components:')
+#        print('Ma', round(fitData3[0], 1), ' ms')
+#        print('Mb', round(fitData3[2], 1), ' ms')
+#        print('Mc', round(fitData3[4], 1), ' ms')
+#        print('T1a', round(fitData3[1]*1e3), ' ms')
+#        print('T1b', round(fitData3[3]*1e3), ' ms')
+#        print('T1c', round(fitData3[5]*1e3), ' ms')
+#        rawData['T13'] = [fitData3[1], fitData3[3], fitData3[5]]
+#        rawData['M3'] = [fitData3[0], fitData3[2], fitData3[4]]
+        
+        # Save data
+        name = saveMyData(rawData)
+        
+        # Plots
+        plt.figure(2, figsize=(5, 5))
+        plt.plot(results[:, 0], results[:, 1], 'o')
+        plt.plot(tIR2, func1(tIR2, *fitData1))
+        plt.plot(tIR2, func2(tIR2, *fitData2))
+#        plt.plot(tIR2, func3(tIR2, *fitData3))
+        plt.title(name)
         plt.xscale('log')
         plt.xlabel('t(ms)')
-        plt.ylabel('FID max (mV)')
-        plt.legend()
+        plt.ylabel('Signal (mV)')
+        plt.legend(['Experimental', 'Fitting 1 component', 'Fitting 2 components'])
+        plt.title(name)
         plt.show()
+        
+
+def func1(x, m, t1):
+    return m*(1-2*np.exp(-x/t1))
+
+def func2(x, ma, t1a, mb, t1b):
+    return ma*(1-2*np.exp(-x/t1a))+mb*(1-2*np.exp(-x/t1b))
+
+def func3(x, ma, t1a, mb, t1b, mc, t1c):
+    return ma*(1-2*np.exp(-x/t1a))+mb*(1-2*np.exp(-x/t1b))+mc*(1-2*np.exp(-x/t1c))
+
+def saveMyData(rawData):
+    # Save data
+    dt = datetime.now()
+    dt_string = dt.strftime("%Y.%m.%d.%H.%M.%S")
+    dt2 = date.today()
+    dt2_string = dt2.strftime("%Y.%m.%d")
+    if not os.path.exists('experiments/acquisitions/%s' % (dt2_string)):
+        os.makedirs('experiments/acquisitions/%s' % (dt2_string))
+    if not os.path.exists('experiments/acquisitions/%s/%s' % (dt2_string, dt_string)):
+        os.makedirs('experiments/acquisitions/%s/%s' % (dt2_string, dt_string)) 
+    rawData['name'] = "%s.%s.mat" % ("IR",dt_string)
+    savemat("experiments/acquisitions/%s/%s/%s.%s.mat" % (dt2_string, dt_string, "IR",dt_string),  rawData)
+    return rawData['name']
 
 if __name__ == "__main__":
     inversion_recovery_TGN()
