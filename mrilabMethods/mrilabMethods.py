d8cb692f97434d1e013ba064e1bf972c10927810
yvives <yolanda.vives@physiomri.com>
2022-04-02 12:35:53 +0200

    JMA: I added the methods getIndex, reorganizeGfactor and fixEchoPosition

--- mrilabMethods/mrilabMethods.py
+++ mrilabMethods/mrilabMethods.py
@@ -6,18 +6,25 @@ Created on Sat Apr  2 11:10:48 2022
 @email: josalggui@i3m.upv.es
 """
 import numpy as np
+from configs.hw_config import Gx_factor
+from configs.hw_config import Gy_factor
+from configs.hw_config import Gz_factor
+
+##############################################################
+##############################################################
+##############################################################
+
 
 def getIndex(etl=1, nPH=1, sweepMode=1):
-    
-    ##########################################################################
-    # Create 'ind' array that give you the order to sweep the k-space phase 
-    # lines along an echo train length.
-    # sweepMode = 0: -kMax to kMax
-    # sweepMode = 1: 0 to kMax
-    # sweepMode = 2: kMax to 0
-    # sweepMode = 3: Niquist modulated method
-    ##########################################################################
-    
+    """"
+    @author: J.M. Algarin, MRILab, i3M, CSIC, Valencia, Spain
+    @email: josalggui@i3m.upv.es
+    Create 'ind' array that give you the order to sweep the k-space phase lines along an echo train length.
+    sweepMode = 0: -kMax to kMax
+    sweepMode = 1: 0 to kMax
+    sweepMode = 2: kMax to 0
+    sweepMode = 3: Niquist modulated method
+    """
     n2ETL=int(nPH/2/etl)
     ind = []
     if nPH==1:
@@ -55,3 +62,54 @@ def getIndex(etl=1, nPH=1, sweepMode=1):
                     ind = np.concatenate((ind, np.arange(nPH-1, 0, -2*n2ETL)-2*ii), axis=0)
 
     return np.int32(ind)
+
+
+##############################################################
+##############################################################
+##############################################################
+
+
+def reorganizeGfactor(axes):
+    """"
+    @author: J.M. Algarin, MRILab, i3M, CSIC, Valencia, Spain
+    @email: josalggui@i3m.upv.es
+    Create an array of 3 elements with the gradient conversion factor corresponding to the given axes order
+    """
+    
+    # Set the normalization factor for readout, phase and slice gradient
+    gFactor = np.array([0., 0., 0.])
+    for ii in range(3):
+        if axes[ii]==0:
+            gFactor[ii] = Gx_factor
+        elif axes[ii]==1:
+            gFactor[ii] = Gy_factor
+        elif axes[ii]==2:
+            gFactor[ii] = Gz_factor
+    
+    return(gFactor)
+
+
+##############################################################
+##############################################################
+##############################################################
+
+
+def fixEchoPosition(echoes, data0):
+    """"
+    @author: J.M. Algarin, MRILab, i3M, CSIC, Valencia, Spain
+    @email: josalggui@i3m.upv.es
+    Oversampled data obtained with a given echo train length and readout gradient only is used here to determine the true position of k=0.
+    After getting the position of k = 0 for each gradient-spin-echo, it shift the sampled data to place k = 0 at the center of each acquisition window.
+    """
+    
+    etl = np.size(echoes, axis=0)
+    n = np.size(echoes, axis=1)
+    idx = np.argmax(np.abs(echoes), axis=1)
+    idx = idx-int(n/2)
+    data1 = data0*0
+    for ii in range(etl):
+        if idx[ii]>0:
+            idx[ii] = 0
+        echoes[ii, -idx[ii]::] = echoes[ii, 0:n+idx[ii]]
+        data1[:, ii, -idx[ii]::] = data0[:, ii, 0:n+idx[ii]]
+    return(data1)
