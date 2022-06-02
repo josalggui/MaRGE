50675385e6a872d764dab587f911b2d95f8e01b6
josalggui <josalggui@i3m.upv.es>
2022-06-02 16:22:11 +0200

    JMA: trial with sequence as a class instead of a script.

--- FirstMRI.py
+++ FirstMRI.py
@@ -27,12 +27,14 @@ from PyQt5.QtWidgets import QApplication
 from controller.sessionviewer_controller import SessionViewerController
 import cgitb 
 cgitb.enable(format = 'text')
+import seq.rare2 as rare
 
 VERSION = "0.1.0"
 AUTHOR = "Yolanda Vives"
 
 print("Graphical User Interface for Magnetic Resonance Imaging")
 
+seq = rare.RARE()
 app = QApplication(sys.argv)
 #    gui = MainViewController()
 gui = SessionViewerController('')
