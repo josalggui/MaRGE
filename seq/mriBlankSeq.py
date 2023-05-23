07e7df30728e369c27b95ee47497dbdb77a8a460
josalggui <josalggui@outlook.com>
2023-05-23 17:20:36 +0200

    [JMA] now cicDelay is from hw_config.py. Also fixed an error in the custom decimate method on mriBlankSeq.py

--- seq/mriBlankSeq.py
+++ seq/mriBlankSeq.py
@@ -447,7 +447,7 @@ class MRIBLANKSEQ:
         self.mapVals['dataOver'] = dataOver
 
         # Decimate the signal after 'fir' filter
-        dataFull = sig.decimate(dataOver[int((hw.cicDelayPoints-1)/2)::], hw.oversamplingFactor, ftype='fir', zero_phase=True)
+        dataFull = sig.decimate(dataOver[int((hw.oversamplingFactor-1)/2)::], hw.oversamplingFactor, ftype='fir', zero_phase=True)
 
         # Remove addRdPoints
         nPoints = int(dataFull.shape[0]/nRdLines)-2*hw.addRdPoints
@@ -518,10 +518,9 @@ class MRIBLANKSEQ:
         It only works with the Experiment class in controller, that inherits from Experiment in marcos_client
         """
         # Generate instructions taking into account the cic filter delay and addRdPoints
-        cicDelayPoints = 3
         samplingRate = self.expt.getSamplingRate() / hw.oversamplingFactor # us
-        t0 = tStart - (hw.addRdPoints*hw.oversamplingFactor-cicDelayPoints) * samplingRate # us
-        t1 = tStart + gateTime + (hw.addRdPoints*hw.oversamplingFactor + cicDelayPoints) * samplingRate
+        t0 = tStart - (hw.addRdPoints * hw.oversamplingFactor - hw.cic_delay_points) * samplingRate # us
+        t1 = tStart + (hw.addRdPoints * hw.oversamplingFactor + hw.cic_delay_points) * samplingRate + gateTime # us
         self.flo_dict['rx%i' % channel][0] = \
             np.concatenate((self.flo_dict['rx%i' % channel][0], np.array([t0, t1])), axis=0)
         self.flo_dict['rx%i' % channel][1] = \
