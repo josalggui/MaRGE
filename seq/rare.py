df232df8d4d50fa568f2cacb35cb9ce0b7d969a5
josalggui <josalggui@outlook.com>
2023-06-21 09:43:57 +0200

    [RARE] Now it is possible to shift the gradient echo respect to the spin echo. Also the image orientation can be unlocked.

--- seq/rare.py
+++ seq/rare.py
@@ -64,6 +64,8 @@ class RARE(blankSeq.MRIBLANKSEQ):
         self.addParameter(key='shimming', string='Shimming (*1e4)', val=[-12.5, -12.5, 7.5], field='OTH')
         self.addParameter(key='parFourierFraction', string='Partial fourier fraction', val=1.0, field='OTH', tip="Fraction of k planes aquired in slice direction")
         self.addParameter(key='freqCal', string='Calibrate frequency', val=1, field='OTH', tip="0 to not calibrate, 1 to calibrate")
+        self.addParameter(key='echo_shift', string='Echo time shift', val=0.0, units=units.us, field='OTH', tip='Shift the gradient echo time respect to the spin echo time.')
+        self.addParameter(key='unlock_orientation', string='Unlock image orientation', val=0, field='OTH', tip='0: Images oriented according to standard. 1: Image raw orientation')
 
     def sequenceInfo(self):
         print("\n3D RARE sequence")
@@ -326,7 +328,7 @@ class RARE(blankSeq.MRIBLANKSEQ):
                     gradAmp[self.axesOrientation[0]] = rdGradAmplitude
                     gradAmp = np.dot(rot, np.reshape(gradAmp, (3, 1)))
                     if (repeIndex==0 or repeIndex>=self.dummyPulses) and dc==False:         # This is to account for dummy pulses
-                        t0 = tEcho-self.rdGradTime/2-gradRiseTime-hw.gradDelay
+                        t0 = tEcho-self.rdGradTime/2-gradRiseTime-hw.gradDelay+self.echo_shift
                         self.gradTrap(t0, gradRiseTime, self.rdGradTime, gradAmp[0], gSteps, 0, self.shimming)
                         self.gradTrap(t0, gradRiseTime, self.rdGradTime, gradAmp[1], gSteps, 1, self.shimming)
                         self.gradTrap(t0, gradRiseTime, self.rdGradTime, gradAmp[2], gSteps, 2, self.shimming)
@@ -334,7 +336,7 @@ class RARE(blankSeq.MRIBLANKSEQ):
 
                     # Rx gate
                     if (repeIndex==0 or repeIndex>=self.dummyPulses):
-                        t0 = tEcho-self.acqTime/2-addRdPoints/BW
+                        t0 = tEcho-self.acqTime/2-addRdPoints/BW+self.echo_shift
                         self.rxGate(t0, self.acqTime+2*addRdPoints/BW)
                         acqPoints += nRD
 
@@ -349,8 +351,7 @@ class RARE(blankSeq.MRIBLANKSEQ):
                     gradAmp[self.axesOrientation[1]] = phGradients[phIndex]
                     gradAmp[self.axesOrientation[2]] = slGradients[slIndex]
                     gradAmp = np.dot(rot, np.reshape(gradAmp, (3, 1)))
-                    # t0 = tEcho+self.acqTime/2+addRdPoints/BW-hw.gradDelay
-                    t0 = tEcho+self.rdGradTime/2+gradRiseTime-hw.gradDelay
+                    t0 = tEcho+self.rdGradTime/2+gradRiseTime-hw.gradDelay+self.echo_shift
                     if (echoIndex<self.etl-1 and repeIndex>=self.dummyPulses):
                         self.gradTrap(t0, gradRiseTime, self.phGradTime, -gradAmp[0], gSteps, 0, self.shimming)
                         self.gradTrap(t0, gradRiseTime, self.phGradTime, -gradAmp[1], gSteps, 1, self.shimming)
@@ -391,6 +392,7 @@ class RARE(blankSeq.MRIBLANKSEQ):
         self.rdDephTime = self.rdDephTime*1e6
         self.inversionTime = self.inversionTime*1e6
         self.preExTime = self.preExTime*1e6
+        self.echo_shift = self.echo_shift*1e6
         nRepetitions = int(nSL*nPH/self.etl)
         scanTime = nRepetitions*self.repetitionTime
         self.mapVals['scanTime'] = scanTime*nSL*1e-6
@@ -642,48 +644,52 @@ class RARE(blankSeq.MRIBLANKSEQ):
             image = np.abs(self.mapVals['image3D'])
             image = image/np.max(np.reshape(image,-1))*100
 
-            # Image orientation
-            if self.axesOrientation[2] == 2:  # Sagittal
-                title = "Sagittal"
-                if self.axesOrientation[0] == 0 and self.axesOrientation[1] == 1:  #OK
-                    image = np.flip(image, axis=2)
-                    image = np.flip(image, axis=1)
-                    xLabel = "A | PHASE | P"
-                    yLabel = "I | READOUT | S"
-                else:
-                    image = np.transpose(image, (0, 2, 1))
-                    image = np.flip(image, axis=2)
-                    image = np.flip(image, axis=1)
-                    xLabel = "A | READOUT | P"
-                    yLabel = "I | PHASE | S"
-            if self.axesOrientation[2] == 1: # Coronal
-                title = "Coronal"
-                if self.axesOrientation[0] == 0 and self.axesOrientation[1] == 2: #OK
-                    image = np.flip(image, axis=2)
-                    image = np.flip(image, axis=1)
-                    image = np.flip(image, axis=0)
-                    xLabel = "R | PHASE | L"
-                    yLabel = "I | READOUT | S"
-                else:
-                    image = np.transpose(image, (0, 2, 1))
-                    image = np.flip(image, axis=2)
-                    image = np.flip(image, axis=1)
-                    image = np.flip(image, axis=0)
-                    xLabel = "R | READOUT | L"
-                    yLabel = "I | PHASE | S"
-            if self.axesOrientation[2] == 0:  # Transversal
-                title = "Transversal"
-                if self.axesOrientation[0] == 1 and self.axesOrientation[1] == 2:
-                    image = np.flip(image, axis=2)
-                    image = np.flip(image, axis=1)
-                    xLabel = "R | PHASE | L"
-                    yLabel = "P | READOUT | A"
-                else:  #OK
-                    image = np.transpose(image, (0, 2, 1))
-                    image = np.flip(image, axis=2)
-                    image = np.flip(image, axis=1)
-                    xLabel = "R | READOUT | L"
-                    yLabel = "P | PHASE | A"
+            if not self.unlock_orientation: # Image orientation
+                if self.axesOrientation[2] == 2:  # Sagittal
+                    title = "Sagittal"
+                    if self.axesOrientation[0] == 0 and self.axesOrientation[1] == 1:  #OK
+                        image = np.flip(image, axis=2)
+                        image = np.flip(image, axis=1)
+                        xLabel = "(-Y) A | PHASE | P (+Y)"
+                        yLabel = "(-X) I | READOUT | S (+X)"
+                    else:
+                        image = np.transpose(image, (0, 2, 1))
+                        image = np.flip(image, axis=2)
+                        image = np.flip(image, axis=1)
+                        xLabel = "(-Y) A | READOUT | P (+Y)"
+                        yLabel = "(-X) I | PHASE | S (+X)"
+                elif self.axesOrientation[2] == 1: # Coronal
+                    title = "Coronal"
+                    if self.axesOrientation[0] == 0 and self.axesOrientation[1] == 2: #OK
+                        image = np.flip(image, axis=2)
+                        image = np.flip(image, axis=1)
+                        image = np.flip(image, axis=0)
+                        xLabel = "(+Z) R | PHASE | L (-Z)"
+                        yLabel = "(-X) I | READOUT | S (+X)"
+                    else:
+                        image = np.transpose(image, (0, 2, 1))
+                        image = np.flip(image, axis=2)
+                        image = np.flip(image, axis=1)
+                        image = np.flip(image, axis=0)
+                        xLabel = "(+Z) R | READOUT | L (-Z)"
+                        yLabel = "(-X) I | PHASE | S (+X)"
+                elif self.axesOrientation[2] == 0:  # Transversal
+                    title = "Transversal"
+                    if self.axesOrientation[0] == 1 and self.axesOrientation[1] == 2:
+                        image = np.flip(image, axis=2)
+                        image = np.flip(image, axis=1)
+                        xLabel = "(+Z) R | PHASE | L (-Z)"
+                        yLabel = "(+Y) P | READOUT | A (-Y)"
+                    else:  #OK
+                        image = np.transpose(image, (0, 2, 1))
+                        image = np.flip(image, axis=2)
+                        image = np.flip(image, axis=1)
+                        xLabel = "(+Z) R | READOUT | L (-Z)"
+                        yLabel = "(+Y) P | PHASE | A (-Y)"
+            else:
+                xLabel = "%s axis" % axesStr[1]
+                yLabel = "%s axis" % axesStr[0]
+                title = "Image"
 
             result1 = {}
             result1['widget'] = 'image'
