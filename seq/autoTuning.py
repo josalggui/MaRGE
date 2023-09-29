fdce884f7fdee47bd706b766bd5324e4f3d39bec
josalggui <josalggui@outlook.com>
2023-09-29 10:22:17 +0200

    [JMA] Fixed a bug in autotuning and fid

--- seq/autoTuning.py
+++ seq/autoTuning.py
@@ -276,6 +276,16 @@ class AutoTuning(blankSeq.MRIBLANKSEQ):
         # Print results
         print("\nS11 = %0.1f dB" % s11_db)
 
+        # Save parameters to source sequence
+        try:
+            sequence = self.sequenceList[self.mapVals['seqName']]
+            sequence.mapVals['matching'] = self.states[stateCm]
+            sequence.mapVals['tuning'] = self.states[stateCt]
+            sequence.mapVals['series'] = self.states[stateCs]
+        except:
+            pass
+
+        # Save result into the mapVals to save the rawData
         self.mapVals['series'] = self.states[stateCs]
         self.mapVals['tuning'] = self.states[stateCt]
         self.mapVals['matching'] = self.states[stateCm]
