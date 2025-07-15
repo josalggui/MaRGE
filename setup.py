23d2cef1eeb0abc8ba48007110364d838ac11fe3
Nihal <nihal.ouahabi@etu.univ-grenoble-alpes.fr>
2025-07-15 11:51:59 +0200

    Version 0.0.5 running from pypi!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="marge-mri-example",
-    version="0.1",
+    version="0.0.5",
     packages=find_packages(),
     install_requires=[],
     author="Nihal Ouahabi",
