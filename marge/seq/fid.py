9267e1f8ecbe3b4a296e6474a6f9ec4fe4a8be2b
José Miguel Algarín <josalggui@i3m.upv.es>
2026-01-07 11:07:05 +0100

    FID and RARE sequences hiden

--- marge/seq/fid.py
+++ marge/seq/fid.py
@@ -31,7 +31,7 @@ class FID(blankSeq.MRIBLANKSEQ):
         super(FID, self).__init__()
         # Input the parameters
         self.addParameter(key='seqName', string='FIDinfo', val='FID')
-        self.addParameter(key='toMaRGE', val=True)
+        self.addParameter(key='toMaRGE', val=False)
         self.addParameter(key='nScans', string='Number of scans', val=1, field='RF')
         self.addParameter(key='larmorFreq', string='Larmor frequency (MHz)', val=3.08, field='RF')
         self.addParameter(key='rfExAmp', string='RF excitation amplitude (a.u.)', val=0.3, field='RF')
