3acdefb95b9b0ba828c68875b33efacbed404733
Marcel Ochsendorf <info@marcelochsendorf.com>
2026-03-05 16:37:25 +0100

    added sequence subfolder support

--- marge/widgets/widget_sequence_list.py
+++ marge/widgets/widget_sequence_list.py
@@ -4,7 +4,7 @@
 @affiliation:MRILab, i3M, CSIC, Valencia, Spain
 """
 from PyQt5.QtWidgets import QComboBox, QSizePolicy
-from marge.seq.sequences import defaultsequences
+from marge.seq.sequences import defaultsequences, sequence_display_names
 
 
 class SequenceListWidget(QComboBox):
@@ -12,8 +12,11 @@ class SequenceListWidget(QComboBox):
         super(SequenceListWidget, self).__init__(*args, **kwargs)
         self.main = parent
 
-        # Add sequences to sequences list
-        self.addItems(sorted(list(defaultsequences.keys())))
+        # Show folder-prefixed labels in the UI, keep raw seq key as item data.
+        keys = sorted(defaultsequences.keys(), key=lambda key: sequence_display_names.get(key, key).lower())
+        for key in keys:
+            display_name = sequence_display_names.get(key, key)
+            self.addItem(display_name, key)
 
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         self.setMaximumWidth(400)
