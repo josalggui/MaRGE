fceb530acc9f3d7a402074473dd0c67270585bd8
Joan Benlloch <joanbenllochgarcia@gmail.com>
2026-03-05 12:49:29 +0100

    Cambios en tyger_config.py

--- marge/marge_tyger/tyger_config.py
+++ marge/marge_tyger/tyger_config.py
@@ -5,5 +5,5 @@ cp_batchsize_RARE = 1000
 yml_petra = 'marge_tyger/petra_art.yml'
 denoising_pipeline = 'marge_tyger/pipeline_denoising.sh'
 snraware_version = 'Local'  # 'None', 'Local', or 'TEP'
-tyger_server = 'https://i3m.tyger.cloud'
-#tyger_server = 'ssh://user@92.185.248.57:50022/opt/tyger/api.sock'
+#tyger_server = 'https://i3m.tyger.cloud'
+tyger_server = 'ssh://jarvis@92.185.248.57:50022/opt/tyger/api.sock'
